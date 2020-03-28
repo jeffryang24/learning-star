b946c07d5e6918d29283e1ca6f54b4a80d71624e
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-03-28 17:00:40 +0700

    feat(gatsbyjs): part 1

--- javascript/gatsbyjs/hello-world/src/pages/about.js
+++ javascript/gatsbyjs/hello-world/src/pages/about.js
@@ -5,9 +5,8 @@ import Header from '../components/header';
 
 export default () => (
   <div style={{ color: 'purple' }}>
-    <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
-    <Header text="Hello World!" />
+    <Link to="/">Home</Link>
+    <Header text="About World" />
     <p>What a world!</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
