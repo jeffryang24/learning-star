9852b0275b7d424799dca7112c970c62b6a60f7e
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-04-04 18:26:56 +0700

    feat(part-8): finishing gatsby part 8

--- javascript/gatsbyjs/hello-world/gatsby-config.js
+++ javascript/gatsbyjs/hello-world/gatsby-config.js
@@ -8,6 +8,22 @@ module.exports = {
   /* Your site config here */
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `My Page`,
+        short_name: `my_page`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
@@ -30,5 +46,7 @@ module.exports = {
   ],
   siteMetadata: {
     title: 'Pandas Eating Lots',
+    description: 'A simple description about pandas eating lots...',
+    author: 'gatsbyjs',
   },
 };
