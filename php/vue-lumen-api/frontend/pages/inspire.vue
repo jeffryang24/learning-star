202a77ed41a85b1592695964c0af10e2d8a0a297
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-04-22 13:25:30 +0700

    feat(php-vue_lumen_api): migrate vue-lumen-api repo into learning star
    old repo: https://github.com/jeffryang24/vue-lumen-api

---
+++ php/vue-lumen-api/frontend/pages/inspire.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      Tes saja - Ini bisa dibaca kalau login
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5">
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+        <code>{{ state }}</code>
+      </blockquote>
+      <v-btn @click="$auth.logout()">Log out</v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  middleware: ['auth'],
+  computed: {
+    state() {
+      console.log(this.$auth)
+      return JSON.stringify(this.$auth.$state, undefined, 2)
+    }
+  }
+}
+</script>
---
+++ php/vue-lumen-api/frontend/pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <v-btn
+        color="info"
+        @click="$auth.loginWith('google')"
+      >Login with Google</v-btn>
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5">
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
