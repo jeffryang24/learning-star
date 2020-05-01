ac024f7e17f6e98da5ec4600d891094e13856419
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-05-02 00:29:50 +0700

    feat(js-gql): finishing mutation exercise

--- javascript/graphql/hackernews-node/src/index.js
+++ javascript/graphql/hackernews-node/src/index.js
@@ -1,18 +1,79 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const links = [
+let links = [
   {
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'How to graphql',
   },
+  {
+    id: 'link-1',
+    url: 'www.howtographql.com',
+    description: 'How to graphql',
+  },
+  {
+    id: 'link-2',
+    url: 'www.howtographql.com',
+    description: 'How to graphql',
+  },
+  {
+    id: 'link-3',
+    url: 'www.howtographql.com',
+    description: 'How to graphql',
+  },
 ];
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     feed: () => links,
     hello: (_, { name }) => `Hello ${name || 'World'}`,
     info: () => 'Hacker hour',
+    link: (_, { id }) => links.find(link => link.id === id),
+  },
+  Mutation: {
+    post: (_, args) => {
+      const { url, description } = args;
+
+      const link = {
+        id: `link-${idCount++}`,
+        url,
+        description,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (_, { id, url, description }) => {
+      const currentIndex = links.findIndex(link => link.id === id);
+      if (currentIndex === -1) {
+        return null;
+      } else {
+        const currentLink = links[currentIndex];
+        const updatedLink = {
+          ...currentLink,
+          url: typeof url === 'undefined' ? currentLink.url : url,
+          description:
+            typeof description === 'undefined'
+              ? currentLink.description
+              : description,
+        };
+        const newArray = [
+          ...links.slice(0, currentIndex),
+          updatedLink,
+          ...links.slice(currentIndex + 1),
+        ];
+        links = newArray;
+        return updatedLink;
+      }
+    },
+    deleteLink: (_, { id }) => {
+      const willBeDeletedLink = links.find(link => link.id === id);
+      if (willBeDeletedLink) {
+        links = links.filter(link => link.id !== id);
+        return willBeDeletedLink;
+      }
+      return null;
+    },
   },
   Link: {
     id: parent => parent.id,
