175086288b41a981fe999b0fefaa684d3f51198a
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-06-21 20:18:01 +0700

    feat(haskell-vacationlabs): add toUpperCase example

--- haskell/vacationlabs/src/Throwaway.hs
+++ haskell/vacationlabs/src/Throwaway.hs
@@ -1,5 +1,7 @@
 module Throwaway where
 
+import           Data.Char                      ( toUpper )
+
 addNumbers :: Int
 addNumbers = 10 + 20
 
@@ -9,3 +11,9 @@ addInts x y = x + y
 
 checkNumber :: Int -> String
 checkNumber x = if mod x 2 == 0 then "even" else "odd"
+
+toUpperCase :: String -> String -> String
+toUpperCase processedString remainingString = if null remainingString
+  then processedString
+  else toUpperCase (processedString ++ [toUpper (head remainingString)])
+                   (tail remainingString)
