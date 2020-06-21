e73369bdf67b99a5b54003a05dcd7324fe5a03e6
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-06-21 18:43:18 +0700

    feat(haskell-vacationlabs): ending throwaway.hs

--- haskell/vacationlabs/src/Throwaway.hs
+++ haskell/vacationlabs/src/Throwaway.hs
@@ -2,3 +2,10 @@ module Throwaway where
 
 addNumbers :: Int
 addNumbers = 10 + 20
+
+addInts :: Int -> Int -> Int
+addInts x y = x + y
+
+
+checkNumber :: Int -> String
+checkNumber x = if mod x 2 == 0 then "even" else "odd"
