c4f220b39ffd87ffdc98a93049cf1f00e415d2d9
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2020-06-28 19:35:31 +0700

    feat(haskell): part 2 exercise 1

--- haskell/vacationlabs/src/Exercise.hs
+++ haskell/vacationlabs/src/Exercise.hs
@@ -14,3 +14,35 @@ multipleSum3Or5 n total
   | n == 0                       = total
   | mod n 3 == 0 || mod n 5 == 0 = multipleSum3Or5 (n - 1) (total + n)
   | otherwise                    = multipleSum3Or5 (n - 1) total
+
+square :: Int -> Int
+square a = a ^ 2
+
+sumOfSquare :: Int -> Int -> Int
+sumOfSquare remainingCount total
+  | remainingCount == 0 = total
+  | otherwise = sumOfSquare (remainingCount - 1) (total + square remainingCount)
+
+squareOfSum :: Int -> Int -> Int
+squareOfSum remainingCount total
+  | remainingCount == 0 = square total
+  | otherwise = squareOfSum (remainingCount - 1) (total + remainingCount)
+
+squareDifference :: Int -> Int
+squareDifference n = abs (squareOfSum n 0 - sumOfSquare n 0)
+
+fibonacci :: Int -> Int
+fibonacci n | n < 1     = 0
+            | n == 1    = 1
+            | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+
+sumEvenFibonacci :: Int -> Int -> Int -> Int
+sumEvenFibonacci maxValue n total
+  | total + nextFibonacci > maxValue = total
+  | mod currentFibonacci 2 == 0 = sumEvenFibonacci maxValue
+                                                   (n + 1)
+                                                   (total + currentFibonacci)
+  | otherwise = sumEvenFibonacci maxValue (n + 1) total
+ where
+  currentFibonacci = fibonacci n
+  nextFibonacci    = fibonacci n + 1
