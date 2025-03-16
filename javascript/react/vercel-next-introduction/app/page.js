9ce0f5d62df6be49dc993f91995fb064ccb06277
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2025-03-16 19:21:52 +0700

    feat(vercel-next): finishing vercel-next learn project (continuation from react)

---
+++ javascript/react/vercel-next-introduction/app/page.js
@@ -0,0 +1,24 @@
+import LikeButton from "./like-button";
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  return (
+    <div>
+      {/* Nesting the Header component */}
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <Header title="React" />
+      <Header title="A new title" />
+      <LikeButton />
+    </div>
+  );
+}
